515a13ca9b95172ee15c5044b7e8dfd5d0e7f5dc
Revathi G <revathikrishna0323@gmail.com>
2022-09-05 21:32:50 +0530

    added source code for Ballad.java

--- src/main/java/com/jap/ballad/Ballad.java
+++ src/main/java/com/jap/ballad/Ballad.java
@@ -1,14 +1,31 @@
 package com.jap.ballad;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Ballad {
-    public String readPoemFromAFile(String fileName){
-       // Write the logic to read from a file
-        return "";
+    public static void main(String[] args) {
+        Ballad ballad = new Ballad();
+        ballad.readPoemFromAFile("src/main/resources/ballad.txt");
     }
 
-
+    public String readPoemFromAFile(String fileName) {
+        String output = "";
+        int lineCount = 0;
+        // Write the logic to read from a file
+        try (FileReader reader = new FileReader(fileName); BufferedReader bufferedReader = new BufferedReader(reader)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                lineCount++;
+                output = output.concat(line + "\n");
+            }
+        } catch (FileNotFoundException ex) {
+            throw new RuntimeException(ex);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return output;
+    }
 }
